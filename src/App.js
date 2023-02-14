b427ef5664edc482f81f0f2f0c342c31c17972a8
Nilay Rudra <109015625+N03Nilay@users.noreply.github.com>
2023-02-14 23:05:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import { useEffect } from 'react';
 
 function App() {
-
-  const {
-    transcript,
-    resetTranscript
-  } = useSpeechRecognition();
-
-  // useEffect(() => {
-  //   SpeechRecognition.startListening({ continuous: true })
-  // },[])
-  const start = () => {
-    SpeechRecognition.startListening({ continuous: true })
-  }
-
   return (
    <>
-    <button onClick={start}>Start</button>
-    <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-    <textarea className ="txt"  value={transcript}></textarea>
+    <h1>Certificate generation</h1>
     </>
   );
 }
