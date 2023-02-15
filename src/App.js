1f0d7cc04f071dbc57cfb1d07dbda7ae98f27921
Suryansh0201 <89742359+Suryansh0201@users.noreply.github.com>
2023-02-16 00:14:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MLogo from './images/sh.png'
+import './App.css';
+import Navbar from './Components/Navbar/navbar';
+import Footer from './Components/Footer/footer';
+function App() {
+  return (
+   <>
+  
+    <Navbar/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
